ab399518b6457bb59fda5f2e007d6d04d8ec8777
Dzung Do <neitdung@gmail.com>
2025-03-19 18:09:28 +0700

    add contract deploy

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition-ethers");
+require('dotenv').config();
+
+module.exports = {
+  solidity: "0.8.22",
+  networks: {
+    ganache: {
+      url: "http://127.0.0.1:3636",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    avax: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    eth: {
+      url: "https://eth-sepolia.public.blastapi.io",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  ignition: {
+    defaultNetwork: "ganache", // Set default deployment network
+  },
+};
